9b154e0e978099fc36d15f370269a4c978c500f2
yaralam <yara.alam@gmail.com>
2026-02-13 14:28:04 -0600

    finished chapter exercises

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 // main.js
 
 console.log("Hello, World!");
+let targetElement = document.querySelector("h2");
+targetElement.addEventListener("mouseover", function() {
+    console.log(this.innerText);
+    this.innerText = "TIME";
+    this.style = "cursor: wait";
+});
+targetElement.addEventListener("mouseout", function () {
+    this.innerText = "IKEA";
+    this.style = "cursor: pointer";
+})
